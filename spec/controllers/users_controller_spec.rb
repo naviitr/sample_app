90d4cd6180f15bdfc0d269cbe3bae845ec3df54f
Naveen Kumar shukla <shukla.iitr@gmail.com>
2015-01-19 09:37:00 +0000

    modifed test script

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,7 +5,7 @@ describe UsersController do
 
   describe "GET 'index'" do
 
-    describe "for non-signed-in users" do
+    describe "for this non-signed-in users" do
       it "should deny access" do
         get :index
         response.should redirect_to(signin_path)
